9959644e2171f1642dac4c48c8d2575a06110f65
Alivia Blount <aliviablount@gmail.com>
2016-10-04 14:46:27 -0700

    in class

--- challenge_2/js/app.js
+++ challenge_2/js/app.js
@@ -1,8 +1,23 @@
 console.log('challenge #2 -- app.js loaded!');
 
-// var app = angular.module("appTwo", []);
-// app.controller("WelcomeController", WelcomeController);
+var app = angular.module("appTwo", []);
+app.controller("WelcomeController", WelcomeController);
 
-// function WelcomeController(){
-//   this.full_name = "YOUR NAME"
-// }
+function WelcomeController(){
+  this.full_name = "Alivia"
+  this.age = "2"
+  this.city = "St. Louis"
+  this.state = "New York"
+}
+
+WelcomeController.prototype.letterCount = function() {
+  // array of two arrays with letters
+  var name = this.full_name.split(" ")
+  var letters = name[0].concat(name[1]);
+  var numLetters = letters.count
+  return numLetters;
+};
+
+WelcomeController.prototype.upperCase = function() {
+  return this.full_name.toUpper() + "!"
+};
